2d489312366e3e2d5828013eee0809e190835993
Andrey Dremin <sankoa230@gmail.com>
2024-06-14 21:39:33 +0300

    refactor: changed card,column dtos sctructures

--- src/cards/dto/update-card.dto.ts
+++ src/cards/dto/update-card.dto.ts
@@ -4,10 +4,6 @@ import { CardStatus } from "@prisma/client";
 export class UpdateCardDto {
     @ApiProperty()
     text:string;
-    @ApiProperty()
-    columnId:string;
-    @ApiProperty()
-    status?:CardStatus
 }
 
 export class ChangeStatusCardDto {
